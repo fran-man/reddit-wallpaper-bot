71bccb640603f50db20ecec35a0053e4eea388fc
fran-man <fran.man@hotmail.co.uk>
2019-07-21 12:16:40 +0100

    Added first test class and added some example json

--- src/main/java/com/franm/wallpaperbot/Requests/SubSearch.java
+++ src/main/java/com/franm/wallpaperbot/Requests/SubSearch.java
@@ -8,6 +8,12 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.client.HttpClient;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.franm.wallpaperbot.reddit.ListingParser;
+
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.io.IOUtils;
+
 @Component
 public class SubSearch{
   private static final Logger LOGGER = LoggerFactory.getLogger(SubSearch.class);
@@ -29,8 +35,11 @@ public class SubSearch{
 	   request.addHeader("User-Agent", "USER_AGENT");
      try{
 	      HttpResponse response = client.execute(request);
-        LOGGER.info(response.getEntity().toString());
-        return response.toString();
+        ListingParser parser = new ListingParser(IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8.name()));
+        for(JsonNode node : parser.extractValuesFromResults("")) {
+        	LOGGER.info(node.asText("InvalidNode..."));
+        }
+        return IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8.name());
       }
       catch (Exception ex){
         LOGGER.error("Error Calling URL!", ex);
---
+++ src/main/java/com/franm/wallpaperbot/reddit/ListingParser.java
@@ -0,0 +1,38 @@
+package com.franm.wallpaperbot.reddit;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.io.IOException;
+
+@Slf4j
+public class ListingParser{
+  private String listingString;
+  private final ObjectMapper objectMapper = new ObjectMapper();
+  public ListingParser(String listingString){
+    this.listingString = listingString;
+  }
+
+  public List<JsonNode> extractValuesFromResults(String key){
+    JsonNode jsonNode = null;
+    try{
+      log.info(this.listingString);
+      jsonNode = this.objectMapper.readTree(this.listingString);
+    }
+    catch(IOException ex){
+      log.error(ex.getMessage());
+    }
+
+    if(jsonNode == null){
+      return new ArrayList<>();
+    }
+    else{
+      return jsonNode.findValues("data");
+    }
+  }
+}

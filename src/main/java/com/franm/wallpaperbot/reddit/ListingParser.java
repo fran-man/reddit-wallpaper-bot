7bffda4b4891d7d6016cf2540d394ef7ca73d06b
fran-man <fran.man@hotmail.co.uk>
2019-07-28 21:27:34 +0100

    Complete unit tests and functionality of getting children

--- src/main/java/com/franm/wallpaperbot/reddit/ListingParser.java
+++ src/main/java/com/franm/wallpaperbot/reddit/ListingParser.java
@@ -1,51 +1,52 @@
 package com.franm.wallpaperbot.reddit;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.extern.slf4j.Slf4j;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import springfox.documentation.spring.web.json.Json;
-
-import java.util.List;
-import java.util.ArrayList;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
-public class ListingParser{
-  private String listingString;
-  private final ObjectMapper objectMapper = new ObjectMapper();
-  public ListingParser(String listingString){
-    this.listingString = listingString;
-  }
+public class ListingParser {
+    private String listingString;
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-  public List<JsonNode> extractValuesFromResults(String key){
-
-    List<JsonNode> children = this.getListingChildren();
-    if(children.size() == 0){
-      return new ArrayList<>();
+    public ListingParser(String listingString) {
+        this.listingString = listingString;
     }
-    else{
-      List<JsonNode> resultNodes = children.stream().flatMap(c -> c.findValues("data").get(0).findValues(key).stream()).filter(c -> c != null).collect(Collectors.toList());
-      return resultNodes.size() == 0 ? new ArrayList<>() : resultNodes;
-    }
-  }
-  
-	private List<JsonNode> getListingChildren() {
-		JsonNode jsonNode = null;
-		try {
-			log.info(this.listingString);
-			jsonNode = this.objectMapper.readTree(this.listingString);
-		} catch (IOException ex) {
-			log.error(ex.getMessage());
-		}
 
-		if (jsonNode == null) {
-			return new ArrayList<>();
-		} else {
-			return jsonNode.findValues("data").get(0).findValues("children");
-		}
-	}
+    public List<JsonNode> extractValuesFromResults(String key) {
+
+        List<JsonNode> children = this.getListingChildren();
+        if (children.size() == 0) {
+            return new ArrayList<>();
+        } else {
+            List<JsonNode> resultNodes = children.stream().flatMap(c -> c.findValues("data").get(0).findValues(key).stream()).filter(c -> c != null).collect(Collectors.toList());
+            return resultNodes.size() == 0 ? new ArrayList<>() : resultNodes;
+        }
+    }
+
+    private List<JsonNode> getListingChildren() {
+        JsonNode jsonNode = null;
+        try {
+            log.info(this.listingString);
+            jsonNode = this.objectMapper.readTree(this.listingString);
+        } catch (IOException ex) {
+            log.error(ex.getMessage());
+        }
+
+        if (jsonNode == null) {
+            return new ArrayList<>();
+        } else {
+            JsonNode childArray = jsonNode.findValues("data").get(0).findValues("children").get(0);
+            ArrayList<JsonNode> result = new ArrayList<>();
+            for(JsonNode child : childArray){
+                result.add(child);
+            }
+            return result;
+        }
+    }
 }

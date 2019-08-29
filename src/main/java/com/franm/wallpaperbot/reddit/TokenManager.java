f88b2963fbeea505208a6fe33a2046ecd8e3ec58
fran-man <fran.man@hotmail.co.uk>
2019-08-29 21:56:35 +0100

    Start building token manager to fetch the token from reddit

--- src/main/java/com/franm/wallpaperbot/reddit/TokenManager.java
+++ src/main/java/com/franm/wallpaperbot/reddit/TokenManager.java
@@ -1,9 +1,13 @@
 package com.franm.wallpaperbot.reddit;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.net.URISyntaxException;
 
 @Component
 @Slf4j
@@ -14,6 +18,8 @@ public class TokenManager {
     private String appSecret;
 
     private String token;
+
+    private static final String ACCESS_TOKEN_URL = "https://www.reddit.com/api/v1/access_token";
 
     @Autowired
     public TokenManager(@Value("${reddit.user}") final String username, @Value("${reddit.pass}") final String pass, @Value("${reddit.appid}") final String appID, @Value("${reddit.appsecret}") final String appSecret) {
@@ -30,6 +36,16 @@ public class TokenManager {
      * @return The token as a string
      */
     public String getToken(){
-
+        URIBuilder bldr = null;
+        try {
+            bldr = new URIBuilder(ACCESS_TOKEN_URL);
+            bldr.setParameter("grant_type", "password").setParameter("username", this.username).setParameter("password", this.pass);
+            HttpPost postReq = new HttpPost(bldr.build());
+        }
+        catch(URISyntaxException ex){
+            log.error("Incorrect URI" + ex.getMessage());
+            return "";
+        }
+        return this.token;
     }
 }

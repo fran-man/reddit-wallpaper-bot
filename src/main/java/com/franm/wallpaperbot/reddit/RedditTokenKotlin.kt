cea8f523190533ad0213f8e5a90b4cf9bed0e650
fran-man <fran.man@hotmail.co.uk>
2019-10-07 22:09:26 +0100

    Update RedditTokenKotlin.kt

--- src/main/java/com/franm/wallpaperbot/reddit/RedditTokenKotlin.kt
+++ src/main/java/com/franm/wallpaperbot/reddit/RedditTokenKotlin.kt
@@ -2,4 +2,6 @@ package com.franm.wallpaperbot.reddit
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class RedditTokenKotlin(@JsonProperty("access_token") var accessToken : String, @JsonProperty("access_token") var accessToken : String, @JsonProperty("access_token") var accessToken : String, @JsonProperty("access_token") var accessToken : String)
+data class RedditTokenKotlin(@JsonProperty("access_token") var accessToken: String, @JsonProperty(
+        "expires_in") var expiry: String, @JsonProperty("scope") var scope: String, @JsonProperty(
+        "token_type") var tokenType: String)

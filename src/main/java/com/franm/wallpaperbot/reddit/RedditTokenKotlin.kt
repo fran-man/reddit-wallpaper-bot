c07fb8c6dbdb178604c1f8b527ecff7d6107c8e8
fran-man <fran.man@hotmail.co.uk>
2019-10-07 22:00:34 +0100

    Create RedditTokenKotlin.kt

---
+++ src/main/java/com/franm/wallpaperbot/reddit/RedditTokenKotlin.kt
@@ -0,0 +1,5 @@
+package com.franm.wallpaperbot.reddit
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class RedditTokenKotlin(@JsonProperty("access_token") var accessToken : String, @JsonProperty("access_token") var accessToken : String, @JsonProperty("access_token") var accessToken : String, @JsonProperty("access_token") var accessToken : String)
